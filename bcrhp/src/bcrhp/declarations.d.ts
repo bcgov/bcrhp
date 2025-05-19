a70d391bac5f3df5c666bc3ffe3f7c919cd46fd5
brett <brett@qedsystems.ca>
2025-05-19 08:32:54 -0700

    Fix declarations import

--- bcrhp/src/bcrhp/declarations.d.ts
+++ bcrhp/src/bcrhp/declarations.d.ts
@@ -1,7 +1,7 @@
 // declare untyped modules that have been added to your project in `package.json`
 // Module homepage on npmjs.com uses logos "TS" or "DT" to indicate if typed
 
-import('@/arches/arches/declarations.d.ts');
+import('@/arches/declarations.d.ts');
 import('@/bcgov_arches_common/declarations.d.ts');
 import('@/arches_component_lab/declarations.d.ts');
 
