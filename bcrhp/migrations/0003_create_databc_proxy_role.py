f561eb165d64f01645665f3f3d813a2b4bb17b1e
brett <brett@qedsystems.ca>
2025-08-01 06:40:27 -0700

    Remove CD substitution variables in SQL scripts

--- bcrhp/migrations/0003_create_databc_proxy_role.py
+++ bcrhp/migrations/0003_create_databc_proxy_role.py
@@ -1,8 +1,12 @@
 from django.db import migrations
 from bcgov_arches_common.migrations.operations.privileged_sql import RunPrivilegedSQL
+from bcrhp.settings import DATABASES
 
 
 class Migration(migrations.Migration):
+    arches_db_user = DATABASES["default"]["USER"]
+    db_databc_user = DATABASES["default"]["DATABC_USERNAME"]
+    db_databc_password = DATABASES["default"]["DATABC_PASSWORD"]
 
     dependencies = [
         ("bcrhp", "0002_alter_crhpexportdata_table"),
@@ -16,45 +20,45 @@ class Migration(migrations.Migration):
         drop schema databc;
     """
 
-    grant_databc_proxy_schema = """
-        grant all privileges on schema databc to {{ arches_db_user }};
+    grant_databc_proxy_schema = f"""
+        grant all privileges on schema databc to {arches_db_user};
     """
 
-    create_databc_proxy_role = """
+    create_databc_proxy_role = f"""
         DO
         $$
             DECLARE
                 databc_role_exists boolean;
             BEGIN
-                select count(*) > 0 into databc_role_exists from pg_roles where rolname = '{{ db_databc_user }}';
+                select count(*) > 0 into databc_role_exists from pg_roles where rolname = '{db_databc_user}';
                 if not databc_role_exists then
-                    Raise NOTICE 'Creating role {{ db_databc_user }}';
-                    create role {{ db_databc_user }} password '{{ db_databc_password }}';
+                    Raise NOTICE 'Creating role {db_databc_user}';
+                    create role {db_databc_user} password '{{ db_databc_password }}';
                 else
-                    Raise NOTICE 'Not creating role {{ db_databc_user }} - it already exists';
+                    Raise NOTICE 'Not creating role {db_databc_user} - it already exists';
                 end if;
-                alter role {{ db_databc_user }} with login;
-                alter role {{ db_databc_user }} set search_path = databc,public;
-                revoke all on schema public from {{ db_databc_user }};
-                grant connect on database {{ arches_db_name }} to {{ db_databc_user }};
-                grant usage on schema databc to {{ db_databc_user }};
-                grant select on geometry_columns TO {{ db_databc_user }};
-                grant select on geography_columns TO {{ db_databc_user }};
-                grant select on spatial_ref_sys TO {{ db_databc_user }};
+                alter role {db_databc_user} with login;
+                alter role {db_databc_user} set search_path = databc,public;
+                revoke all on schema public from {db_databc_user};
+                grant connect on database {arches_db_name} to {{ db_databc_user }};
+                grant usage on schema databc to {db_databc_user};
+                grant select on geometry_columns TO {db_databc_user};
+                grant select on geography_columns TO {db_databc_user};
+                grant select on spatial_ref_sys TO {db_databc_user};
             END
         $$ language plpgsql;
     """
 
-    drop_databc_proxy_role = """
+    drop_databc_proxy_role = f"""
         DO
         $$
             DECLARE
                 databc_role record;
             BEGIN
-                for databc_role in (select rolname from pg_roles where rolname = '{{ db_databc_user }}') loop
-                    Raise NOTICE 'Dropping role {{ db_databc_user }} and all associated grants';
-                    drop owned by {{ db_databc_user }} cascade;
-                    drop role {{ db_databc_user }};
+                for databc_role in (select rolname from pg_roles where rolname = '{db_databc_user}') loop
+                    Raise NOTICE 'Dropping role {db_databc_user} and all associated grants';
+                    drop owned by {db_databc_user} cascade;
+                    drop role {db_databc_user};
                 end loop;
             END
         $$ language plpgsql;
--- bcrhp/pkg/business_data/resource_views/0003_databc_proxy_grants.sql
+++ bcrhp/pkg/business_data/resource_views/0003_databc_proxy_grants.sql
@@ -1,3 +1,11 @@
 -- Schema databc and DataBC proxy object must be created prior to running this script
-grant execute on function databc.get_first_address(p_resourceinstanceid uuid) to {{ db_databc_user }};
-grant select on databc.V_HISTORIC_ENVIRO_ONEROW_SITE to {{ db_databc_user }};
+DO
+$$
+    declare
+        databc_username text;
+    begin
+        databc_username := replace(current_database(), 'bcrhp', 'databc');
+        EXECUTE format('execute grant execute on function databc.get_first_address(p_resourceinstanceid uuid) to %I;', databc_username);
+        EXECUTE format('grant select on databc.V_HISTORIC_ENVIRO_ONEROW_SITE to %I;', databc_username);
+    end
+$$ language plpgsql;
