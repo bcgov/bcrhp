02afe9627b7579e0531c2401cf669df7e1aa28b7
brett <brett@qedsystems.ca>
2025-08-02 09:37:32 -0700

    Fixing GH CI/CD - Disambiguate alias

--- bcrhp/migrations/sql/2024/2024-11-29_mv_chronology.sql
+++ bcrhp/migrations/sql/2024/2024-11-29_mv_chronology.sql
@@ -3,8 +3,8 @@ select resourceinstanceid,
        start_year,
        end_year,
        dates_approximate,
-       __arches_get_concept_label(chronology) event,
-       information_source->'en'->>'value' source,
-       chronology_notes->'en'->>'value' event_notes
+       __arches_get_concept_label(chronology) as event,
+       information_source->'en'->>'value' as source,
+       chronology_notes->'en'->>'value' as event_notes
 from heritage_site.chronology;
 create index mv_chron_idx on mv_chronology(resourceinstanceid);
