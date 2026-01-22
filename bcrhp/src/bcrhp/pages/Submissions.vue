848c9f407d87d8babc01aa0ac34d091d55c25b16
brett <brett@qedsystems.ca>
2026-01-21 21:14:08 -0800

    Add plugin, use common card component in LG submissions page

--- bcrhp/src/bcrhp/pages/Submissions.vue
+++ bcrhp/src/bcrhp/pages/Submissions.vue
@@ -1,24 +1,24 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import Card from 'primevue/card';
 import Panel from 'primevue/panel';
 import Fluid from 'primevue/fluid';
 import { useGettext } from 'vue3-gettext';
+import Card from '@/bcgov_arches_common/components/card/card.vue';
 
 import { routeNames } from '@/bcrhp/routes.ts';
 
 const { $gettext } = useGettext();
 const workflowItems = ref([
     {
-        label: $gettext('Create New Heritage Property'),
-        description: $gettext('Add a new site'),
+        label: $gettext('Submit Heritage Property'),
+        description: $gettext('Submit a new Heritage Property'),
         icon: 'fa fa-file',
         class: 'dashboard-card ipa',
         routeName: routeNames.newSite,
     },
     {
         label: $gettext('Update Heritage Property'),
-        description: $gettext('Update an existing Site'),
+        description: $gettext('Update an existing heritage property'),
         icon: 'fa fa-file',
         class: 'dashboard-card ipa',
         routeName: routeNames.updateSite,
@@ -38,114 +38,25 @@ const workflowItems = ref([
         class="full-height"
     >
         <Fluid>
-            <div class="flex max-w-2xl">
+            <div class="dashboard-div-flex">
                 <Card
                     v-for="item in workflowItems"
                     :key="item.routeName"
+                    :label="$gettext(item.label)"
+                    :description="$gettext(item.description)"
+                    :subtitle="$gettext(item.description)"
+                    :icon="item.icon"
                     :class="item.class"
-                >
-                    <template #title>
-                        <p class="dashboard-card-title mt-2">
-                            {{ item.label }}
-                        </p>
-                    </template>
-                    <template #content>
-                        <i
-                            :class="item.icon"
-                            class="dashboard-card-icon"
-                        >
-                        </i>
-                    </template>
-                    <template #footer>
-                        <router-link
-                            :to="{ name: item.routeName }"
-                            class="card-router-link"
-                        >
-                            <div class="dashboard-card-link">
-                                <p class="dashboard-card-description">
-                                    {{ item.description }}
-                                </p>
-                            </div>
-                        </router-link>
-                    </template>
-                </Card>
+                    :route="{ name: item.routeName }"
+                />
             </div>
         </Fluid>
     </Panel>
 </template>
 
 <style scoped>
-.dashboard-card {
-    /*margin: 1rem;*/
-    min-width: 33%;
-    /*display: inline-block;*/
-}
-
-.card-router-link {
-    text-decoration: none;
-}
-
-.dashboard-card.ipa {
-    border: 1px solid #7847ce;
-    border-radius: 0%;
-    height: 252px;
-    max-width: 275px;
-    margin: 5px;
-    text-align: center;
-    background: #8bc3eb;
-    color: #440ea2;
-    opacity: 0.8;
-}
-
-.dashboard-card.ipa :deep(.p-card-body) {
-    padding: 0rem;
-}
-
-.dashboard-card.ipa:hover {
-    background: #6da8d3;
-}
-
-.dashboard-card-title {
-    font-weight: 500;
-    color: #440ea2;
-    text-align: center;
-    overflow-wrap: break-word;
-}
-
-.dashboard-card-icon {
-    color: #fff;
-    font-size: 2.8rem;
-    line-height: 32px;
-    padding: 1rem;
-    margin-bottom: 3rem;
-}
-
-.dashboard-card-description {
-    height: 55px;
-    width: 100%;
-    font-weight: 600;
-    border-top: 1px solid #7847ce;
-    color: white;
-    font-size: 0.7rem;
-}
-
-.dashboard-card-link {
-    background: #2b4e6d;
-}
-
-.dashboard-card-link:hover {
-    background: #4683b9;
-}
-
-.dashboard-card-footer {
-    height: 50px;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    color: white;
-}
-
-.dashboard-card.collection-event {
-    border: solid thick rgba(50, 50, 50, 0.7);
+.dashboard-div-flex {
+    display: flex;
+    flex-wrap: wrap;
 }
 </style>
