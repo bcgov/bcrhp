2ad462cf812d1ef637970c96ab43e5b43c6e6d03
brett <brett@qedsystems.ca>
2026-03-03 11:11:41 -0800

    127 - Fix Heritage Site details page

--- bcrhp/media/js/reports/bcrhp-site.js
+++ bcrhp/media/js/reports/bcrhp-site.js
@@ -3,7 +3,7 @@ import ko from 'knockout';
 import koMapping from 'knockout-mapping';
 import MapReportViewModel from 'viewmodels/bcrhp-site';
 import MapHeader from 'reports/map-header';
-import defaultSiteTemplate from 'templates/views/report-templates/details/heritage_site.htm';
+import defaultSiteTemplate from 'templates/views/report-templates/bcrhp_default.htm';
 export default ko.components.register('bcrhp-site-report', {
     viewModel: MapReportViewModel,
     template: defaultSiteTemplate,
