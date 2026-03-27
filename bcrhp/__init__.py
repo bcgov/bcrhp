55ccfacdbe2f7aad2ab3637891962cbd75e549f5
Philip Hansen <philip.pt.hansen@gmail.com>
2026-03-27 17:21:21 -0600

    version fallback

--- bcrhp/__init__.py
+++ bcrhp/__init__.py
@@ -1,3 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__package__)
+try:
+    __version__ = importlib.metadata.version(__package__)
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
