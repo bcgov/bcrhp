79d62fdd0884cd158cee47801aad318fa416355d
brett <brett@qedsystems.ca>
2026-04-22 17:23:21 -0700

    Apply restrictive permissions to LG group

--- bcrhp/migrations/0206_guest_no_access_admin_nodegroups.py
+++ bcrhp/migrations/0206_guest_no_access_admin_nodegroups.py
@@ -1,20 +1,42 @@
 from django.contrib.auth.models import Group, User
 from django.core.cache import caches
 from django.db import migrations
+from django.db.models import Q
 from guardian.shortcuts import assign_perm, remove_perm
 
 from arches.app.models.models import Node
 
-groups_to_change = ["Guest", "Local Government"]
+lg_pairs = [
+    ("heritage_site", "internal_remark"),
+    ("heritage_site", "site_record_admin"),
+    ("heritage_site_historical_data", "decision_history_n1"),
+    ("lg_person", "contact_information"),
+    ("lg_person", "government_association"),
+    ("lg_person", "government_person_name"),
+    ("lg_person", "username"),
+    ("project_sandbox", "project_boundary"),
+    ("project_sandbox", "sandcastle_project"),
+]
+
+guest_pairs = [
+    ("heritage_site", "internal_remark"),
+    ("heritage_site", "site_record_admin"),
+]
 
 
-def forwards(apps, schema_editor):
-    groups = Group.objects.filter(name__in=groups_to_change).all()
-    nodegroups = [
-        Node.objects.get(alias="internal_remark").nodegroup,
-        Node.objects.get(alias="site_record_admin").nodegroup,
-    ]
-    for nodegroup in nodegroups:
+def get_nodegroups(pairs):
+    query = Q()
+    for slug, alias in pairs:
+        query |= Q(graph__slug=slug, alias=alias)
+
+    nodes = list(Node.objects.filter(query).select_related("graph"))
+    return [node.nodegroup for node in nodes]
+
+
+def guest_forwards(apps, schema_editor):
+    groups = Group.objects.filter(name="Guest").all()
+
+    for nodegroup in get_nodegroups(guest_pairs):
         for group in groups:
             assign_perm("no_access_to_nodegroup", group, nodegroup)
     user_permission_cache = caches["user_permission"]
@@ -22,12 +44,31 @@ def forwards(apps, schema_editor):
         user_permission_cache.clear()
 
 
-def backwards(apps, schema_editor):
-    groups = Group.objects.filter(name__in=groups_to_change).all()
-    nodegroups = [
-        Node.objects.get(alias="internal_remark").nodegroup,
-        Node.objects.get(alias="site_record_admin").nodegroup,
-    ]
+def guest_backwards(apps, schema_editor):
+    groups = Group.objects.filter(name="Guest").all()
+    nodegroups = get_nodegroups(guest_pairs)
+    for nodegroup in nodegroups:
+        for group in groups:
+            remove_perm("no_access_to_nodegroup", group, nodegroup)
+    user_permission_cache = caches["user_permission"]
+    if user_permission_cache:
+        user_permission_cache.clear()
+
+
+def lg_forwards(apps, schema_editor):
+    groups = Group.objects.filter(name="Local Government").all()
+
+    for nodegroup in get_nodegroups(lg_pairs):
+        for group in groups:
+            assign_perm("no_access_to_nodegroup", group, nodegroup)
+    user_permission_cache = caches["user_permission"]
+    if user_permission_cache:
+        user_permission_cache.clear()
+
+
+def lg_backwards(apps, schema_editor):
+    groups = Group.objects.filter(name="Local Government").all()
+    nodegroups = get_nodegroups(lg_pairs)
     for nodegroup in nodegroups:
         for group in groups:
             remove_perm("no_access_to_nodegroup", group, nodegroup)
@@ -43,4 +84,7 @@ class Migration(migrations.Migration):
         ("guardian", "0002_generic_permissions_index"),
     ]
 
-    operations = [migrations.RunPython(forwards, backwards)]
+    operations = [
+        migrations.RunPython(guest_forwards, guest_backwards),
+        migrations.RunPython(lg_forwards, lg_backwards),
+    ]
