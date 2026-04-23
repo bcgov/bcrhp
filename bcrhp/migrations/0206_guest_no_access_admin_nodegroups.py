421a7469b4a278259710d578f012cec33d6d39fe
brett <brett@qedsystems.ca>
2026-04-22 20:56:58 -0700

    Make node array names clearer

--- bcrhp/migrations/0206_guest_no_access_admin_nodegroups.py
+++ bcrhp/migrations/0206_guest_no_access_admin_nodegroups.py
@@ -6,7 +6,7 @@ from guardian.shortcuts import assign_perm, remove_perm
 
 from arches.app.models.models import Node
 
-lg_pairs = [
+lg_nodes = [
     ("heritage_site", "internal_remark"),
     ("heritage_site", "site_record_admin"),
     ("heritage_site_historical_data", "decision_history_n1"),
@@ -18,7 +18,7 @@ lg_pairs = [
     ("project_sandbox", "sandcastle_project"),
 ]
 
-guest_pairs = [
+guest_nodes = [
     ("heritage_site", "internal_remark"),
     ("heritage_site", "site_record_admin"),
 ]
@@ -36,7 +36,7 @@ def get_nodegroups(pairs):
 def guest_forwards(apps, schema_editor):
     groups = Group.objects.filter(name="Guest").all()
 
-    for nodegroup in get_nodegroups(guest_pairs):
+    for nodegroup in get_nodegroups(guest_nodes):
         for group in groups:
             assign_perm("no_access_to_nodegroup", group, nodegroup)
     user_permission_cache = caches["user_permission"]
@@ -46,7 +46,7 @@ def guest_forwards(apps, schema_editor):
 
 def guest_backwards(apps, schema_editor):
     groups = Group.objects.filter(name="Guest").all()
-    nodegroups = get_nodegroups(guest_pairs)
+    nodegroups = get_nodegroups(guest_nodes)
     for nodegroup in nodegroups:
         for group in groups:
             remove_perm("no_access_to_nodegroup", group, nodegroup)
@@ -58,7 +58,7 @@ def guest_backwards(apps, schema_editor):
 def lg_forwards(apps, schema_editor):
     groups = Group.objects.filter(name="Local Government").all()
 
-    for nodegroup in get_nodegroups(lg_pairs):
+    for nodegroup in get_nodegroups(lg_nodes):
         for group in groups:
             assign_perm("no_access_to_nodegroup", group, nodegroup)
     user_permission_cache = caches["user_permission"]
@@ -68,7 +68,7 @@ def lg_forwards(apps, schema_editor):
 
 def lg_backwards(apps, schema_editor):
     groups = Group.objects.filter(name="Local Government").all()
-    nodegroups = get_nodegroups(lg_pairs)
+    nodegroups = get_nodegroups(lg_nodes)
     for nodegroup in nodegroups:
         for group in groups:
             remove_perm("no_access_to_nodegroup", group, nodegroup)
